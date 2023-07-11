9cac9d27a8992f8cdfe65a45156209a59310c537
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-07-11 14:39:48 -0300

    Tela Home

--- App/page/Home/page_front.py
+++ App/page/Home/page_front.py
@@ -10,46 +10,76 @@ class PageFront(InterfaceHomeFront):
         sg.theme_background_color(self.__color)
 
     def _front_end(self):
-        barra_notificacao = [sg.Image(filename="App//img//barra de notificacao.png")]
+        barra_notificacao = [
+            sg.Image(filename="App//img//barra de notificacao.png", pad=(0, (0, 100)))
+        ]
 
         logo = [
             sg.Image(
                 filename="App//img//logo.png",
                 background_color=self.__color,
-                pad=(70, (0, 0)),
+                pad=(70, (0, 50)),
             )
         ]
 
         welcome_back = [
             sg.Text(
                 "Welcome back",
-                font="Jaldi 20 bold",
+                font="Jaldi 19 bold",
                 background_color=self.__color,
                 text_color="#FFA620",
-                pad=(20, (0, 20)),
-            )
+                pad=(0, (0, 40)),
+            ),
+            sg.Text("", background_color=self.__color, pad=(78, (0, 40))),
         ]
 
-        email = [
+        email = (
             [
-                sg.Image(
-                    filename="App//img//email.png",
-                    background_color=self.__color,
-                    pad=(20, (0, 5)),
-                ),
-                sg.Input(
-                    "E-mail/Phone Number",
-                    size=(31, 10),
-                    background_color=self.__color,
-                    font="Jaldi 13",
-                    pad=(0, (0, 5)),
-                    border_width=0,
-                    text_color="#535353",
-                ),
+                [
+                    sg.Text(
+                        "Email/Phone Number",
+                        font="Jaldi 13 bold",
+                        background_color=self.__color,
+                        text_color="#FFA620",
+                        pad=(0, (0, 10)),
+                    ),
+                    sg.Text("", background_color=self.__color, pad=(80, (0, 10))),
+                ],
+                [
+                    sg.Image(
+                        filename="App//img//email.png",
+                        background_color=self.__color,
+                        pad=(20, (0, 5)),
+                    ),
+                    sg.Input(
+                        "",
+                        size=(25, 10),
+                        background_color=self.__color,
+                        font="Jaldi 13",
+                        pad=(0, (0, 5)),
+                        border_width=0,
+                        text_color="#535353",
+                    ),
+                    sg.Image(
+                        filename="App//img//invisivel.png",
+                        background_color=self.__color,
+                        pad=(30, (0, 5)),
+                    ),
+                ],
+                [sg.HSep(pad=(20, (0, 20)))],
             ],
-            [sg.HSep(pad=(20, (0, 0)))],
-        ]
+        )
         password = [
+            [
+                sg.Text(
+                    "Password",
+                    font="Jaldi 13 bold",
+                    background_color=self.__color,
+                    text_color="#FFA620",
+                    pad=(0, (0, 10)),
+                ),
+                sg.Text("", background_color=self.__color, pad=(120, (0, 10))),
+            ],
             [
                 sg.Image(
                     filename="App//img//password.png",
@@ -72,27 +102,81 @@ class PageFront(InterfaceHomeFront):
                     pad=(30, (0, 5)),
                 ),
             ],
-            [sg.HSep(pad=(20, (0, 0)))],
+            [sg.HSep(pad=(20, (0, 15)))],
         ]
-        remember_me_and_forget_password = []
-        login = []
+        remember_me_and_forget_password = [
+            [
+                sg.Checkbox(
+                    "Remember me",
+                    background_color=self.__color,
+                    checkbox_color="white",
+                    font="Jaldi 10 ",
+                    text_color="#FFA620",
+                    pad=(20, (0, 0)),
+                ),
+                sg.Text("", background_color=self.__color, pad=(30,(0,0))),
+                sg.Text(
+                    "Forget Password?",
+                    font="Jaldi 10 ",
+                    background_color=self.__color,
+                    text_color="#FFA620",
+                    pad=(20, (0, 0)),
+                    justification='r'
+                ),
+            ]
+        ]
+        login = [
+            sg.Button(
+                "Login",
+                size=(13, 1),
+                border_width=0,
+                pad=(0, (50, 15)),
+                font="Jaldi 20 bold",
+                button_color="#FF9900",
+                mouseover_colors="black",
+            )
+        ]
         social_media = [
-            [],
+            [
+                sg.Text(
+                    "Or Login with",
+                    font="Jaldi 10",
+                    background_color=self.__color,
+                    text_color="#FFA620",
+                    pad=(46, (0, 15)),
+                )
+            ],
             [
                 sg.Image(
                     filename="App//img//facebook 1.png",
                     background_color=self.__color,
-                    pad=(0, (0, 0)),
+                    pad=(20, (0, 0)),
                 ),
                 sg.Image(
                     filename="App//img//instagram 1.png",
                     background_color=self.__color,
-                    pad=(0, (0, 0)),
+                    pad=(20, (0, 0)),
                 ),
                 sg.Image(
                     filename="App//img//linkedin.png",
                     background_color=self.__color,
-                    pad=(0, (0, 0)),
+                    pad=(20, (0, 0)),
+                ),
+            ],
+            [
+                sg.Text(
+                    "Dont't have account?",
+                    font="Jaldi 10",
+                    background_color=self.__color,
+                    text_color="#FFA620",
+                    pad=(0, (20, 0)),
+                ),
+                sg.Text(
+                    "Sign up",
+                    font="Jaldi 10 bold",
+                    background_color=self.__color,
+                    text_color="#FFA620",
+                    pad=(0,(20,0))
                 ),
             ],
         ]
@@ -110,7 +194,13 @@ class PageFront(InterfaceHomeFront):
             rodape_sign_up,
         ]
 
-        window = sg.Window("Solmed", layout=layout, size=(390, 844))
+        window = sg.Window(
+            "Solmed",
+            layout=layout,
+            size=(390, 844),
+            element_justification="c",
+            icon="App//img//sol.ico",
+        )
 
         while True:
             event, values = window.read()
